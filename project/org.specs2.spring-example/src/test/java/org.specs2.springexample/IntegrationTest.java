1199f6dfbc8938ce6460ff4d8b135a639a106a03
Jan Machacek <janmachacek@Jans-iMac.local>
2011-09-02 08:54:33 +0100

    Demonstration of multiple DSs, topics, queues

--- project/org.specs2.spring-example/src/test/java/org.specs2.springexample/IntegrationTest.java
+++ project/org.specs2.spring-example/src/test/java/org.specs2.springexample/IntegrationTest.java
@@ -13,14 +13,20 @@ import java.lang.annotation.RetentionPolicy;
  * @author janmachacek
  */
 @Jndi(
-		dataSources = @DataSource(name = "java:comp/env/jdbc/test",
-				driverClass = JDBCDriver.class, url = "jdbc:hsqldb:mem:test"),
+		dataSources = {
+				@DataSource(name = "java:comp/env/jdbc/test",
+					driverClass = JDBCDriver.class, url = "jdbc:hsqldb:mem:test"),
+				@DataSource(name = "java:comp/env/jdbc/external",
+					driverClass = JDBCDriver.class, url = "jdbc:hsqldb:mem:external")
+		},
 		mailSessions = @MailSession(name = "java:comp/env/mail/foo"),
 		transactionManager = @TransactionManager(name = "java:comp/TransactionManager"),
 		jms = @Jms(
 				connectionFactoryName = "java:comp/env/jms/connectionFactory",
-				queues = {@JmsQueue(name = "java:comp/env/jms/requests"), @JmsQueue(name = "java:comp/env/jms/responses")},
-				topics = {@JmsTopic(name = "java:comp/env/jms/cacheFlush"), @JmsTopic(name = "java:comp/env/jms/ruleUpdate")}
+				queues = {@JmsQueue(name = "java:comp/env/jms/requests"),
+						  @JmsQueue(name = "java:comp/env/jms/responses")},
+				topics = {@JmsTopic(name = "java:comp/env/jms/cacheFlush"),
+						  @JmsTopic(name = "java:comp/env/jms/ruleUpdate")}
 		),
 		workManagers = @WorkManager(name = "java:comp/env/work/WorkManager", kind = WorkManager.Kind.CommonJ)
 )
