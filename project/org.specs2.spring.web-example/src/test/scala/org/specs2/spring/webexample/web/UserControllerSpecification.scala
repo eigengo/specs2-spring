adefa3f2f10eab944bf2fc4f11be74035eaea092
Jan Machacek <janmachacek@Jans-iMac.local>
2011-09-28 17:04:11 +0100

    Web example cleanup

--- project/org.specs2.spring.web-example/src/test/scala/org/specs2/spring/webexample/web/UserControllerSpecification.scala
+++ project/org.specs2.spring.web-example/src/test/scala/org/specs2/spring/webexample/web/UserControllerSpecification.scala
@@ -13,27 +13,18 @@ import org.specs2.spring.web._
   webContextLocations = Array("/WEB-INF/sw-servlet.xml"),
   contextLocations = Array("classpath*:/META-INF/spring/module-context.xml"))
 @Transactional
-class IndexControllerTest extends Specification {
+class UserControllerSpecification extends Specification {
 
   @Autowired
   var managementService: ManagementService = _
 
   "web roundtrip test" in {
-    this.managementService.findAll(classOf[User]).isEmpty must beTrue
-
     Xhtml(post)("/users.html", Map("username" -> "aaaa", "fullName" -> "Jan"))
 
     val wo = Xhtml(get)("/users/1.html")
-    wo.model(classOf[User]).getFullName must_== ("Jan")
-    wo.model(classOf[User]).getUsername must_== ("aaaa")
-
-    val raw = Raw(get)("/users/1.html")
-    println(raw!)
-
-    val x = (wo!) >> ("#username")
-    println(x)
-
-    success
+    (wo.model(classOf[User]).getFullName must_== ("Jan"))  ^
+    (wo.model(classOf[User]).getUsername must_== ("aaaa")) ^
+    (((wo!) >>! ("#username")) must_== ("aaaa"))
   }
 
 }
