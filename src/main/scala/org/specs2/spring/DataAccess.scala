7884e3a04986e8af1ec496f17844a6ef800d079f
Jan Machacek <janmachacek@janmachacek.local>
2012-01-31 10:04:31 +0000

    Comments

--- src/main/scala/org/specs2/spring/DataAccess.scala
+++ src/main/scala/org/specs2/spring/DataAccess.scala
@@ -1,6 +1,5 @@
 package org.specs2.spring
 
-import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.orm.hibernate3.{HibernateCallback, HibernateTemplate}
 import org.hibernate.{HibernateException, SessionFactory, Session}
 import org.specs2.execute.{Success, Result}
@@ -8,7 +7,6 @@ import org.specs2.execute.{Success, Result}
 /**
  * @author janmachacek
  */
-
 trait SqlDataAccess {
 
 
@@ -46,16 +44,13 @@ trait HibernateDataAccess {
    * Removes all entities of the given type
    *
    * @param entity implicitly supplied class manifest of the entity type to be deleted
+   * @param sessionFactory the session factory that will have the entities removed
    */
   def deleteAll[T](implicit entity: ClassManifest[T], sessionFactory: SessionFactory) {
     inSession(sessionFactory) { s =>
       s.createQuery("delete from " + entity.erasure.getName).executeUpdate()
     }
   }
-/*  def deleteAll[T](implicit entity: ClassManifest[T], hibernateTemplate: HibernateTemplate) {
-    deleteAll(entity, hibernateTemplate.getSessionFactory)
-  }
-*/
 
   import org.specs2.execute._
 
@@ -64,6 +59,8 @@ trait HibernateDataAccess {
    * ``|>`` function.
    * Typical usage is
    * <pre>
+   *  implicit var sessionFactory = make-SessionFactory-instance()
+   *
    *  "Some service operation" in {
    *    "age" | "name" | "teamName" |
    *     32   ! "Jan"  ! "Wheelers" |
@@ -77,17 +74,16 @@ trait HibernateDataAccess {
    * @return function that inserts the object and returns Success when the insert succeeds.
    */
   def insert[T](implicit sessionFactory: SessionFactory): (T => Result) = {
-    t => inSession(sessionFactory) { s => s.saveOrUpdate(t); Success("ok") }
+    t => inSession(sessionFactory) { s => s.saveOrUpdate(t); Success("ok") } 
   }
-  /*
-  def insert[T](implicit hibernateTemplate: HibernateTemplate): (T => Result) = insert(hibernateTemplate.getSessionFactory)
-  */
 
   /**
    * Returns a function that runs the supplied function f on the object; then inserts the object and returns Success;
    * the function can be supplied to the BeanTables ``|>`` function.<br/>
    * Typical usage is
    * <pre>
+   *  implicit var sessionFactory = make-SessionFactory-instance()
+   *
    *  "Some service operation" in {
    *    "age" | "name" | "teamName" |
    *     32   ! "Jan"  ! "Wheelers" |
@@ -102,27 +98,79 @@ trait HibernateDataAccess {
    * @param f function that operates on the instance ``T``; this function will run before the Hibernate save.
    * @return function that inserts the object and returns Success when the insert succeeds.
    */
-  def insert[T, R](f: T => R)(implicit sessionFactory: SessionFactory): (T => Result) = {
-    {t =>
+  def insert[T, R](f: T => R)(implicit sessionFactory: SessionFactory): (T => Result) = { 
+    t =>
       f(t)
       inSession(sessionFactory) { _.saveOrUpdate(t) }
       Success("ok")
     }
-  }
-  /*
-  def insert[T, R](f: T => R)(implicit hibernateTemplate: HibernateTemplate): (T => Result) = insert(hibernateTemplate.getSessionFactory)
-  */
 
 }
 
 trait HibernateTemplateDataAccess {
   
+  /**
+   * Returns a function that runs the supplied function f on the object; then inserts the object and returns Success;
+   * the function can be supplied to the BeanTables ``|>`` function.<br/>
+   * Typical usage is
+   * <pre>
+   *  implicit var hibernateTemplate = make-HibernateTemplate-instance()
+   *
+   *  "Some service operation" in {
+   *    "age" | "name" | "teamName" |
+   *     32   ! "Jan"  ! "Wheelers" |
+   *     30   ! "Ani"  ! "Team GB"  |> insert[Rider] { r: Rider => r.addEntry(...) }
+   *
+   *    // tests that rely on the inserted Rider objects; each with one Entry inserted in the function given
+   *    // to the insert[Rider] method
+   *    success
+   *  }
+   *  </pre>
+   *
+   * @param f function that operates on the instance ``T``; this function will run before the Hibernate save.
+   * @return function that inserts the object and returns Success when the insert succeeds.
+   */
   def insert[T, R](f: T => R)(implicit hibernateTemplate: HibernateTemplate): (T => Result) = {
-    t => hibernateTemplate.saveOrUpdate(f(t)); Success("ok");
+    t =>
+      f(t)
+      hibernateTemplate.saveOrUpdate(t) 
+      Success("ok");
   } 
 
+  /**
+   * Returns a function that inserts the object and returns Success; the function can be supplied to the BeanTables
+   * ``|>`` function.
+   * Typical usage is
+   * <pre>
+   *  implicit var hibernateTemplate = make-HibernateTemplate-instance()
+   *
+   *  "Some service operation" in {
+   *    "age" | "name" | "teamName" |
+   *     32   ! "Jan"  ! "Wheelers" |
+   *     30   ! "Ani"  ! "Team GB"  |> insert[Rider]
+   *
+   *    // tests that rely on the inserted Rider objects
+   *    success
+   *  }
+   *  </pre>
+   *
+   * @return function that inserts the object and returns Success when the insert succeeds.
+   */
   def insert[T](implicit hibernateTemplate: HibernateTemplate): (T => Result) = {
     t => hibernateTemplate.saveOrUpdate(t); Success("ok");
   } 
+
+  /**
+   * Removes all entities of the given type
+   *
+   * @param entity implicitly supplied class manifest of the entity type to be deleted
+   * @param hibernateTemplate HibernateTemplate instance that will have the entities removed
+   */
+  def deleteAll[T](implicit entity: ClassManifest[T], hibernateTemplate: HibernateTemplate) {
+    hibernateTemplate.execute(new HibernateCallback[Int] {
+      def doInHibernate(session: Session) = 
+        session.createQuery("delete from " + entity.erasure.getName).executeUpdate()
+    })
+  }
 
 }
