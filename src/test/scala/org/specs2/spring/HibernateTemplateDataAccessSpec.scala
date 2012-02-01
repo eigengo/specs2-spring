f0a92c52ea0ec0fc9cc9b4f5cd591e852aa138da
Anirvan Chakraborty <anirvanc@cakesolutions.net>
2012-02-01 14:07:45 +0000

    Added tests for Hibernate3 based data access

---
+++ src/test/scala/org/specs2/spring/HibernateTemplateDataAccessSpec.scala
@@ -0,0 +1,33 @@
+package org.specs2.spring
+
+import domain.User
+import org.specs2.mock.Mockito
+import org.springframework.orm.hibernate3.HibernateTemplate
+
+/**
+ * @author anirvanchakraborty
+ */
+class HibernateTemplateDataAccessSpec extends Specification with HibernateTemplateDataAccess with BeanTables with Mockito {
+  implicit val template = mock[HibernateTemplate]
+
+  "hibernate3 based " in {
+    "insert works as expected" in {
+      "username" | "firstName" |
+        "doo"     !! "bar"       |
+        "doo"     !! "bar"       |> insert[User]
+
+      there were two (template).saveOrUpdate(new User {username="doo"; firstName="bar"})
+    }
+
+    "deleteAll works as expected" in {
+      val users =
+      "username" | "firstName" |
+        "doo"     !! "bar"       |
+        "doo"     !! "bar"       |< classOf[User]
+      deleteAll[User]
+//      there were two (template).execute <-- Need to find what's best to test here
+      success
+    }
+  }
+  
+}
